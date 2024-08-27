05f626d19acaa5e33d777a944cf312f06320ddd9
zeynep152 <zeynep200598@gmail.com>
2024-08-27 23:28:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import React, {useState} from "react";
+import React, {Component, useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0, 
+      step: 1
+    };
+  }
 
-  const increase = () => {
-    setCount(count + step);
+  increase = () => {
+    this.setState({
+      count: this.state.count + this.state.step
+    });
   };
 
-  const decrease = () => {
-    setCount(count - step);
+  decrease = () => {
+    this.setState({
+      count: this.state.count - this.state.step
+    });
   };
 
-  const stepChange = (event) => {
-    setStep(Number(event.target.value));
+  stepChange = (event) => {
+    this.setState({
+      step: Number(event.target.value) 
+    });
   };
-  
-  return (
-    <div className="App">
-      <h1>Counter App</h1>
-      <div>
-        <h2>{count}</h2>
-        <button onClick={increase}>+</button>
-        <button onClick={decrease}>-</button>
-        <input 
-          value={step} 
-          onChange={stepChange}>
-        </input>
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <div>
+          <h2>{this.state.count}</h2>
+          <button onClick={this.increase}>+</button>
+          <button onClick={this.decrease}>-</button>
+          <input 
+            value={this.state.step} 
+            onChange={this.stepChange}>
+          </input>
+        </div>
       </div>
-    </div>
-  );
-}
+    )
+  }
+};
 
 export default App;
